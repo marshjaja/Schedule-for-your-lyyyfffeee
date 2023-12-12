5faf5d6970bd044feedefad0775528fe496dde60
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-12-12 13:11:13 +0000

    Add implemented initial style of app and displayed the date formatted

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+dayjs.extend(window.dayjs_plugin_advancedFormat);
+$(document).ready(function () {
+	var currentDay = $("#currentDay");
+	var todaysDay = dayjs().format("dddd, MMMM Do YYYY");
+	currentDay.text(todaysDay);
+	console.log(todaysDay);
+});
