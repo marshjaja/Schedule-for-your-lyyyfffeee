04029e4eb94c029cb05cc2886fc2235e7e0b2044
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-12-12 14:24:10 +0000

    Add functionality to display color code for past current and future time in the textarea updates every 15 minutes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
+// jQuery Plugin to display the date formatted with suffix
 dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+// Display the date of today formatted as day, months, date with suffix, year
 $(document).ready(function () {
 	var currentDay = $("#currentDay");
 	var todaysDay = dayjs().format("dddd, MMMM Do YYYY");
 	currentDay.text(todaysDay);
 	console.log(todaysDay);
 });
+
+function updateHour() {
+	// Display current hour
+	var currentHour = parseInt(dayjs().format("H"));
+
+	// Color-code each time-block based on past, present, and future when the time-block is viewed.
+	$(".time-block").each(function () {
+		var blockHour = parseInt($(this).attr("id").split("-")[1]);
+		var $textarea = $(this).find(".description");
+
+		if (blockHour < currentHour) {
+			// Past hours(s)
+			$(this).removeClass("present future").addClass("past");
+			$textarea.removeClass("present future").addClass("past");
+		} else if (blockHour === currentHour) {
+			// Current hour
+			$(this).removeClass("past future").addClass("present");
+			$textarea.removeClass("past future").addClass("present");
+		} else {
+			// Future hour(s)
+			$(this).removeClass("past present").addClass("future");
+			$textarea.removeClass("past present").addClass("future");
+		}
+	});
+}
+updateHour();
+
+// Update hour ever 15 minutes
+setInterval(function () {
+	updateHour();
+}, 15 * 60 * 1000);
