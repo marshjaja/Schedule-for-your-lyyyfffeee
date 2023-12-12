ceb2e23e9ce2f9aa3a229ab993219ce55555c0a7
marshjaja <114920895+marshjaja@users.noreply.github.com>
2023-12-12 14:50:03 +0000

    Add localstorage functionality for time block by clicking the save button

--- js/script.js
+++ js/script.js
@@ -35,6 +35,22 @@ function updateHour() {
 }
 updateHour();
 
+$(".saveBtn").on("click", function () {
+	var value = $(this).siblings(".description").val();
+	var time = $(this).parent().attr("id");
+
+	console.log("value:", value);
+	console.log("time:", time);
+
+	// Saving values into localStorage with a key based on selected day and time ID
+	localStorage.setItem(time, value);
+});
+
+$(".time-block").each(function () {
+	var hourId = $(this).attr("id");
+	var storedValue = localStorage.getItem(hourId);
+	$(this).find(".description").val(storedValue);
+});
 // Update hour ever 15 minutes
 setInterval(function () {
 	updateHour();
